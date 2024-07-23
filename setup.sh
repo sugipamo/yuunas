020efa0545a6c47963b92883a9b7ee5623897bae
sugipamo <hakaisin11@gmail.com>
2024-07-23 23:44:20 +0900

    ipv6対応

--- setup.sh
+++ setup.sh
@@ -7,13 +7,24 @@ sudo apt upgrade -y
 # Gitのインストール
 sudo apt install -y git
 
+# Gitリポジトリのクローン
+REPO_URL="https://github.com/sugipamo/yuunas"
+CLONE_DIR="$HOME/yuunas_work"
+
+# クローン先ディレクトリが存在しない場合のみクローン
+if [ ! -d "$CLONE_DIR" ]; then
+    git clone $REPO_URL $CLONE_DIR
+else
+    echo "Directory $CLONE_DIR already exists. Skipping git clone."
+fi
+
 # OpenSSHサーバーのインストール
 sudo apt install -y openssh-server
 sudo systemctl start ssh
 sudo systemctl enable ssh
 
 # インターネット接続が確認できるネットワークインターフェースを取得
-INTERFACE="enp0s3" 
+INTERFACE="enp0s3"
 
 # 静的IPの設定
 NETPLAN_CONFIG="/etc/netplan/99_config.yaml"
@@ -27,26 +38,29 @@ network:
   version: 2
   ethernets:
     $INTERFACE:
-      addresses: [192.168.1.70/24]
+      dhcp4: false
+      addresses:
+        - 192.168.0.100/24
+      gateway4: 192.168.0.1
       nameservers:
-        addresses: [192.168.1.1, 8.8.8.8, 8.8.4.4]
+        addresses:
+          - 192.168.0.1
+          - 8.8.8.8
+          - 8.8.4.4
+      dhcp6: false
+      addresses:
+        - 2001:db8::100/64
+      gateway6: 2001:db8::1
+      nameservers:
+        addresses:
+          - 2001:4860:4860::8888
+          - 2001:4860:4860::8844
 EOF"
 
-chmod 600 $NETPLAN_CONFIG
+chmod 644 $NETPLAN_CONFIG
 
 # 設定の適用
 sudo netplan apply
-
-# Gitリポジトリのクローン
-REPO_URL="https://github.com/sugipamo/yuunas"
-CLONE_DIR="~/yuunas_work"
-
-# クローン先ディレクトリが存在しない場合のみクローン
-if [ ! -d "$CLONE_DIR" ]; then
-    git clone $REPO_URL $CLONE_DIR
-else
-    echo "Directory $CLONE_DIR already exists. Skipping git clone."
-fi
 
 # sshdの設定変更
 SSHD_CONFIG="/etc/ssh/sshd_config"
@@ -54,15 +68,25 @@ SSHD_CONFIG="/etc/ssh/sshd_config"
 # 設定のバックアップ
 sudo cp $SSHD_CONFIG ${SSHD_CONFIG}.bak
 
-# 設定の変更
-sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/' $SSHD_CONFIG
-sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' $SSHD_CONFIG
-sudo sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/' $SSHD_CONFIG
-sudo sed -i 's/#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/' $SSHD_CONFIG
+# IPv6のみSSHを受け付けるように設定
+sudo bash -c "cat >> $SSHD_CONFIG <<EOF
+# SSH over IPv6 only
+Port 2458
+AddressFamily inet6
+ListenAddress ::
+
+PermitRootLogin no
+PasswordAuthentication yes
+PermitEmptyPasswords no
+ChallengeResponseAuthentication no
+EOF"
 
 # 設定を反映
 sudo systemctl restart ssh
 
 # UFWの設定
-sudo ufw allow OpenSSH
+sudo ufw allow 2458/tcp
+sudo ufw allow from any to any port 2458 proto tcp
 sudo ufw enable
+
+echo "Setup complete. Please log out and log back in for Docker group changes to take effect."
