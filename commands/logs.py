8cbfcd36dcc253d299f05c29604fc55c7615199d
sugipamo <hakaisin11@gmail.com>
2024-07-24 21:45:22 +0900

    commandsにlogs.pyを追加

---
+++ commands/logs.py
@@ -0,0 +1,12 @@
+import subprocess
+import sys
+
+def execute():
+    """Docker Compose upを実行する"""
+    try:
+        subprocess.call(
+            "docker-compose logs",
+            shell=True,
+        )
+    except subprocess.CalledProcessError as e:
+        print(e.stderr, file=sys.stderr)
